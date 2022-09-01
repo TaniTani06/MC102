14997b4cad4b889bb9ff65f51b11290bdfee83b9
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2022-09-01 12:28:09 -0300

    Added Aula03.py file

---
+++ Aula03.py
@@ -0,0 +1,7 @@
+#if, if else,
+#flag: deinir uma variável no início do código e mudar seu valor com "ifs"
+
+
+if (True):
+    print("Hello world!")
+    #blocos, comandos que serão executados caso a condição seja satisfeita
