dcfd3975c3f8c97761a0b9b567f98a8119ab0ff6
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2022-09-04 13:54:08 -0300

    modified lab03.py

--- lab03.py
+++ lab03.py
@@ -14,29 +14,29 @@ s = input()
 c = input()
 
 # valor do ingresso inteiro
-preco_vespertino = [30,15,15,15,20,20,30]
-preco_noturno = [30,20,20,30,30,40,40]
-desconto_vespertino = [0.7,0.5,0.5,0.5,0.5,0.5,0.8]
-desconto_noturno = [0.7,0.5,0.5,0.5,0.5,0.7,0.8]
+pv = [30,15,15,15,20,20,30]
+pn = [30,20,20,30,30,40,40]
+dv = [0.7,0.5,0.5,0.5,0.5,0.5,0.8]
+dn = [0.7,0.5,0.5,0.5,0.5,0.7,0.8]
 
 #checar horário
 if((time==18 and min<30) or (time<18)):
-    ingresso = preco_vespertino[day]
+    ingresso = pv[day]
     
     if(s=="S"):
         ingresso = ingresso/2
     else:
         if(c=="C"):
-            ingresso = preco_vespertino[day]*desconto_vespertino[day]
+            ingresso = pv[day]*dv[day]
 
 if((time==18 and min>=30) or (time>18)):
-    ingresso = preco_noturno[day]
+    ingresso = pn[day]
 
     if(s=="S"):
         ingresso = ingresso/2
     else:
         if(c=="C"):
-            ingresso = preco_noturno[day]*desconto_noturno[day]
+            ingresso = pn[day]*dn[day]
 
 # saída do valor do ingresso
 print('Valor do ingresso: R$', format(ingresso, '.2f').replace('.', ','))
