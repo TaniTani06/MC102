33772a4a154a37dcef50015ec985a6235c4dd699
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2022-11-26 12:24:54 -0300

    Modified lab13.py file

--- lab13.py
+++ lab13.py
@@ -6,7 +6,20 @@
 ###################################################
 
 # Leitura de dados
-
-
+candidatos = {"Branco": 0, "Nulo": 0}
+while True:
+    x = str(input())
+    if x == '0':
+        break
+    if x in candidatos.keys():
+        candidatos[x] += 1
+    else:
+        candidatos[x] = 1
 
 # Saída de dados
+a = sorted(candidatos.items(), key=lambda x: x[1], reverse=True)
+for i in a:
+    if (str(i[0]) != 'Branco') and (str(i[0]) != 'Nulo'):
+        print(str(i[0]) + ' ' + str(i[1]))
+print('Brancos ' + str(candidatos['Branco']))
+print('Nulos ' + str(candidatos['Nulo']))
